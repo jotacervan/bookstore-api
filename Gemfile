26b2707c60b52413ce90b0ce32df023c603203a7
jotacervanumanni <jota@umanni.com.br>
2019-12-15 23:13:59 -0300

    Finish github webhooks controller

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'puma', '~> 3.11'
 # gem 'bcrypt', '~> 3.1.7'
 gem 'github_webhook', '~> 1.1'
 gem 'octokit'
+gem 'faker'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -1,24 +1,37 @@
 class GithubWebhooksController < ActionController::Base
+  skip_before_action :verify_authenticity_token
   include GithubWebhook::Processor
 
-  # Handle push event
-  def github_push(payload)
-    puts '****************************************************'
-    puts '****************************************************'
-    puts '****************************************************'
-    puts payload
+  # receive only IssuesEvent from github
+  def github_issues(payload)
+    case payload[:action]
+    when 'deleted'
+      destroy_issue(payload[:issue])
+    else
+      handle_issue(payload[:issue])
+    end
+    render json: {}, status: :ok
   end
 
-  # Handle create event
-  def github_create(payload)
-    puts '****************************************************'
-    puts '****************************************************'
-    puts '****************************************************'
-    puts payload
+  # Handle create, and update event
+  def handle_issue(issue)
+    author = Author.find_by(github_id: issue[:id])
+    if author.present?
+      author.update(name: issue[:title], bio: issue[:body] )
+    else
+      author = Author.create(name: issue[:title], github_id: issue[:id], from_github: true, bio: issue[:body])
+      author.books.create(title: Faker::Book.title, publisher: author, price: 24.20)
+    end
+    author
+  end
+
+  # handle destroy event
+  def destroy_issue(issue)
+    author = Author.find_by(github_id: issue[:id])
+    author.destroy if author.present?
   end
 
   private
-
   def webhook_secret(payload)
     ENV['GITHUB_WEBHOOK_SECRET']
   end
